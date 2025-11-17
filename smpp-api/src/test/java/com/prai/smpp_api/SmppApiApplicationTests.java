105e00851a5acb1b22750b37ea67bfa50e914359
PRASHANNA RAI <prai931024@gmail.com>
2025-11-17 16:58:54 +0100

    Testing code bootstrap

---
+++ smpp-api/src/test/java/com/prai/smpp_api/SmppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prai.smpp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
